6981aabe4a209607b25df9856e237db77c91d727
ggjiny <jnss599@gmail.com>
2024-02-09 22:38:27 +0900

    feat: array rendering, ordering, delete

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import FoodList from "./FoodList";
+import mockItems from "../mock.json";
+import { useState } from "react";
+
+function App() {
+  const [items, setItems] = useState(mockItems);
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewstClick = () => setOrder("createdAt");
+  const handleCalorieClick = () => setOrder("calorie");
+
+  const handleDelete = (id) => {
+    const newItems = items.filter((item) => item.id !== id);
+    setItems(newItems);
+  };
+
+  return (
+    <div>
+      <button onClick={handleNewstClick}>최신순</button>
+      <button onClick={handleCalorieClick}>칼로리순</button>
+      <FoodList items={sortedItems} onDelete={handleDelete} />
+    </div>
+  );
+}
+
+export default App;
